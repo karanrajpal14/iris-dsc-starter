8234a96476ae7cca6216eea3c7fd1da41c452b20
Karan Rajpal <rajpal.karan73@gmail.com>
2020-10-18 17:57:43 -0500

    Removed redundant code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Linear Regression using Iris Dataset | Google DSC`,
+    description: `A SPA that connects to Google Cloud to perform Linear Regression and predict the species of Iris genus based on the input`,
+    author: `@karanrajpal14`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +18,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `iris-regression-spa-google-dsc-uta`,
+        short_name: `iris-classifier-spa`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/iris-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ],
 }
