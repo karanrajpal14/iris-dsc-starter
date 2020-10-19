6385ba7ce0346cd1af5138d536b537e69e85c375
Karan Rajpal <rajpal.karan73@gmail.com>
2020-10-19 11:13:41 -0500

    Added image component

--- src/components/image.js
+++ src/components/image.js
@@ -2,35 +2,42 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
- */
+const Image = ({ filename }) => {
+  let imageNode = <></>
 
-const Image = () => {
   const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+    {
+      allImageSharp {
+        nodes {
+          fluid(maxWidth: 900, traceSVG: { color: "#639" }) {
+            originalName
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
           }
         }
       }
     }
   `)
 
-  if (!data?.placeholderImage?.childImageSharp?.fluid) {
+  if (!data?.allImageSharp?.nodes) {
     return <div>Picture not found</div>
   }
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  const image = data.allImageSharp.nodes.find(n => {
+    return n.fluid.originalName.includes(filename)
+  })
+
+  if (filename !== "") {
+    imageNode = (
+      <div className="mt-4">
+        <figure className="image is-128x128 is-inline-block">
+          <Img alt={filename} fluid={image.fluid} />
+          <figcaption>{filename}</figcaption>
+        </figure>
+      </div>
+    )
+  }
+
+  return imageNode
 }
 
 export default Image
